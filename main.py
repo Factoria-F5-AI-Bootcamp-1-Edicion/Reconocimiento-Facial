0c00731555a437994014d8f11992869080129422
= <=>
2023-01-24 11:31:08 +0100

    Gitignore más guía para el reconocimiento facial

---
+++ main.py
@@ -0,0 +1,6 @@
+from recognition import FaceRecognition
+# pip install cmake dlib==19.22 -- pip install https://github.com/jloh02/dlib/releases/download/v19.22/dlib-19.22.99-cp310-cp310-win_amd64.whl
+
+if __name__ == '__main__':
+    fr = FaceRecognition()
+    fr.run_recognition()
